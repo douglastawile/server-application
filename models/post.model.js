d60b5a41519982bd108bd44d0b1ce0605f8fc647
Douglas Tawile <douglastawile@gmail.com>
2024-08-10 15:04:11 +0000

    Updated the model

--- models/post.model.js
+++ models/post.model.js
@@ -7,11 +7,11 @@ const PostSchema = new mongoose.Schema(
       required: [true, "Title is required."],
       trim: true,
       minlength: [4, "Title should not be less than 4 letters."],
-      maxlength: [20, "Title should not be more than 20 letters."],
+      maxlength: [100, "Title should not be more than 100 letters."],
     },
     content: {
       type: String,
-      required: [true, "Content is required."],
+      required: [true, "Conte0nt is required."],
       trim: true,
       minlength: [4, "Content should not be less than 4 letters."],
       maxlength: [2000, "Content should not be more than 2000 letters."],
